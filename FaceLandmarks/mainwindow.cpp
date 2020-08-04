983e89e638a1ed9ba57d3e098ca1dc8648d62b56
adem0zer <aoz406kou@gmail.com>
2020-08-04 13:49:03 +0300

    FaceLandmarks

---
+++ FaceLandmarks/mainwindow.cpp
@@ -0,0 +1,116 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QObject>
+#include <QTimer>
+#include <QProcess>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    timer = new QTimer(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+
+void MainWindow::on_btnOpenCamera_clicked()
+{
+    cap.open(0);
+
+    if(!cap.isOpened())  // Check if we succeeded
+    {
+        cout << "camera is not open" << endl;
+    }
+    else
+    {
+        cout << "camera is open" << endl;
+        detector = get_frontal_face_detector();
+        deserialize("/home/adem/FileFolderPri/shape_predictor_68_face_landmarks.dat") >> shape_model;
+
+        connect(timer, SIGNAL(timeout()), this, SLOT(update_window()));
+        timer->start(20);
+
+        ui->btnCloseCamera->setEnabled(true);
+
+    }
+}
+
+void MainWindow::on_btnCloseCamera_clicked()
+{
+    disconnect(timer, SIGNAL(timeout()), this, SLOT(update_window()));
+    cap.release();
+
+    Mat image = Mat::zeros(frame.size(),CV_8UC3);
+
+    show_frame(image);
+
+    cout << "camera is closed" << endl;
+}
+
+
+void MainWindow::update_window()
+{
+    cap >> frame;
+
+    // convert openCV cv::Mat image to dlib::array2d:
+    array2d<bgr_pixel> dlib_image;
+    assign_image(dlib_image, cv_image<bgr_pixel>(frame));
+
+    std::vector<dlib::rectangle> detected_faces = detector(dlib_image);
+
+    int number_of_detected_faces = detected_faces.size();
+
+    cout << "Number of detected faces : " << number_of_detected_faces << endl;
+
+    std::vector<full_object_detection> shapes;
+
+    //int rect_x, rect_y, rect_w, rect_h;
+
+    for (int i = 0; i < number_of_detected_faces; i++)
+    {
+        full_object_detection shape = shape_model(dlib_image, detected_faces[i]);
+
+        shapes.push_back(shape);
+/*
+        rect_x = detected_faces[i].left();
+        rect_y = detected_faces[i].top();
+        rect_w = detected_faces[i].right() - rect_x;
+        rect_h = detected_faces[i].bottom() - rect_y;
+
+        Rect face_rectangle(rect_x, rect_y, rect_w, rect_h);
+
+        cv::rectangle(frame, face_rectangle, Scalar( 0, 255, 0 ), 3, 8, 0);
+*/
+        for (int j = 0; j < 68; j++)
+        {
+            circle(frame, Point(shape.part(j).x(), shape.part(j).y()), 2, Scalar( 0, 0, 255 ), 1, LINE_AA );
+        }
+    }
+
+    show_frame(frame);
+}
+
+void MainWindow::show_frame(Mat &image)
+{
+    Mat resized_image = image.clone();
+
+    int width_of_label = ui->lblCamera->width();
+    int height_of_label = ui->lblCamera->height();
+
+    Size size(width_of_label, height_of_label);
+
+    cv::resize(image, resized_image, size);
+
+    cvtColor(resized_image, resized_image, COLOR_RGB2BGR);
+
+    QImage qt_image((const unsigned char*) (resized_image.data), resized_image.cols, resized_image.rows, QImage::Format_RGB888);
+
+    ui->lblCamera->setPixmap(QPixmap::fromImage(qt_image));
+}
+
---
+++ FaceLandmarks/mainwindow.h
@@ -0,0 +1,58 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+#include <iostream>
+
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+
+#include <dlib/opencv.h>
+#include <dlib/image_io.h>
+#include <dlib/gui_widgets.h>
+#include <dlib/image_processing.h>
+#include <dlib/image_processing/frontal_face_detector.h>
+#include <dlib/image_processing/render_face_detections.h>
+
+
+using namespace std;
+using namespace cv;
+using namespace dlib;
+
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+
+    void update_window();
+
+    void on_btnOpenCamera_clicked();
+
+    void on_btnCloseCamera_clicked();
+
+private:
+    Ui::MainWindow *ui;
+
+    QTimer *timer;
+    VideoCapture cap;
+
+    Mat frame;
+    QImage qt_image;
+
+    frontal_face_detector detector;
+    shape_predictor shape_model;
+
+    void show_frame(Mat &);
+};
+#endif // MAINWINDOW_H
